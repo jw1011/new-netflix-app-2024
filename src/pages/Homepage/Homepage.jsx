b4108ff2140a089a9518ae45b86f41263b0e73e5
Juwon Lee <a8316010@gmail.com>
2024-01-18 20:40:44 +0900

    create popular movies list

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Banner from "./components/Banner/Banner";
+import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 
+//1. 배너 - popular 1순위 영화 불러오기
+//2. popular list
+//3. top rated list
+//4. upcoming list
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Banner />
+      <PopularMovieSlide />
+    </div>
+  );
 };
 
 export default Homepage;
---
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+import MovieSlider from "../../../../common/MovieSlider/MovieSlider";
+import { responsive } from "../../../../constants/responsive";
+
+const PopularMovieSlide = () => {
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+  if (isLoading) {
+    <Spinner animation="border" variant="danger">
+      loading...
+    </Spinner>;
+  }
+  if (isError) {
+    <Alert variant="danger">{error.message}</Alert>;
+  }
+  return (
+    <div>
+      <MovieSlider
+        title="Popular Movies"
+        movies={data.results}
+        responsive={responsive}
+      />
+    </div>
+  );
+};
+
+export default PopularMovieSlide;
